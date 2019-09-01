e2fdb07991dc14439c7702614bcae9e15f6bd485
Jairo Honorio <registrofx@gmail.com>
2019-08-31 22:14:34 -0300

    Extract quiz Result to another file

--- test_project/lib/Quiz.dart
+++ test_project/lib/Quiz.dart
@@ -5,22 +5,22 @@ import 'Answer.dart';
 import 'Question.dart';
 
 class Quiz extends StatelessWidget {
-  void Function() answerQuestion;
+  void Function(int score) answerQuestion;
   List<Map<String, Object>> questions;
 
   int question_index;
 
-  Quiz(void Function() this.answerQuestion, List<Map<String, Object>> this.questions, int this.question_index);
+  Quiz(void Function(int score) this.answerQuestion, List<Map<String, Object>> this.questions, int this.question_index);
 
 
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
       Question(questions[question_index]["question"]),
-      ...(questions[question_index]["answers"] as List<String>)
+      ...(questions[question_index]["answers"] as List<Map<String, Object>>)
           .map((answer) => new Answer(
-        pressHandler: answerQuestion,
-        text: answer,
+        pressHandler: () => answerQuestion(answer['score']),
+        text: answer['text'],
       ))
           .toList(),
     ]);
--- test_project/lib/main.dart
+++ test_project/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'Quiz.dart';
+import 'Result.dart';
 
 Future main() async {
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
@@ -9,21 +10,11 @@ Future main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -51,59 +33,61 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  var _question_index = 0;
+  var _questionIndex = 0;
+  var _totalScore = 0;
 
   final questions = [
     {
       "question": "Fav color?",
-      "answers": ["Red", "Blue", "Green"]
+      "answers": [
+        {'text': 'Red', 'score': 5},
+        {'text': 'Blue', 'score': 10},
+        {'text': 'Green', 'score': 8}
+      ]
     },
     {
       "question": "Fav animal?",
-      "answers": ["Dog", "Cat", "Mouse"]
+      "answers": [
+        {'text': 'Dog', 'score': 10},
+        {'text': 'Cat', 'score': 1},
+        {'text': 'Mouse', 'score': 0}
+      ]
     }
   ];
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
-  void _answerQuestion() {
-    print("Answer question!");
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    print("Answer question! score: $_totalScore");
     setState(() {
-      _question_index++;
+      _questionIndex++;
+    });
+  }
+
+  void resetQuiz(){
+    setState(() {
+      _totalScore = 0;
+      _questionIndex = 0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Column(
         children: [
-          _question_index < questions.length
-              ? Quiz(_answerQuestion, questions, _question_index)
-              : Center(
-                  child: Text("Good"),
-                ),
+          _questionIndex < questions.length
+              ? Quiz(_answerQuestion, questions, _questionIndex)
+              : Result(_totalScore, resetQuiz,),
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -125,8 +109,6 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-
