eb6f617d25bf7f2b2e07b449367a8f0276172240
Jairo Honorio <registrofx@gmail.com>
2019-09-08 21:24:24 -0300

    Initial transactions list

--- test_project/lib/main.dart
+++ test_project/lib/main.dart
@@ -29,15 +29,87 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: '1', title: 'Snickers', amount: 4.20, dateTime: DateTime.now())
+  ];
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Text('Hello World'),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: Container(
+              child: Text('Chart'),
+              width: double.infinity,
+            ),
+            elevation: 5,
+          ),
+          Column(
+            children: transactions.map((transaction) {
+              return Card(
+                  child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      transaction.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.green,
+                      ),
+                    ),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.green,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        transaction.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        transaction.dateTime.toString(),
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                  ),
+                ],
+              ));
+            }).toList(),
+          )
+        ],
+      ),
     );
   }
+}
+
+class Transaction {
+  @required
+  String id;
+  @required
+  String title;
+  @required
+  double amount;
+  @required
+  DateTime dateTime;
+
+  Transaction({this.id, this.title, this.amount, this.dateTime});
 }
